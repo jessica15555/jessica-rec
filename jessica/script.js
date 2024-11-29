d44cb1a47378010c58fc438aaf46dacca65fd41f
jessi <109973663+jessica15555@users.noreply.github.com>
2024-11-29 10:31:50 -0300

    Add files via upload

---
+++ jessica/script.js
@@ -0,0 +1,4 @@
+// Função para mostrar uma promoção ao clicar no botão
+document.getElementById('promoButton').addEventListener('click', function() {
+    alert('Confira nossas promoções incríveis! Até 50% de desconto em produtos selecionados!');
+});
